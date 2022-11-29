535526e1de31c18bf5fe03c217f5927ac8fcb3b7
Moh Dev <73759551+MohamedCS1@users.noreply.github.com>
2022-11-29 15:25:25 +0100

    add drawable res

--- app/src/main/java/com/example/a2048game/MainActivity.kt
+++ app/src/main/java/com/example/a2048game/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.a2048game
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
         setContentView(R.layout.activity_main)
-
-
-
     }
 
 
